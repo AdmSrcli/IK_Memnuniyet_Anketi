a735f2731dc0c68efd24a526470ae947d4aa976f
Adem SARAÇLI <adem.saracli@hotmail.com>
2024-10-04 13:45:20 +0300

    Ik_Memnuniyet_Anketi
    %100 memnuniyet Garantili

---
+++ Zam/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace Zam
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            btnevet = new Button();
+            btnhayir = new Button();
+            label3 = new Label();
+            pictureBox2 = new PictureBox();
+            progressBar1 = new ProgressBar();
+            lblmemnuniyet = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold);
+            label1.ForeColor = Color.Blue;
+            label1.Location = new Point(354, 370);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 39);
+            label1.TabIndex = 0;
+            label1.Text = "SORU:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft Sans Serif", 22.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(513, 370);
+            label2.Name = "label2";
+            label2.Size = new Size(124, 42);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // btnevet
+            // 
+            btnevet.BackColor = Color.White;
+            btnevet.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold);
+            btnevet.Location = new Point(1049, 654);
+            btnevet.Name = "btnevet";
+            btnevet.Size = new Size(179, 68);
+            btnevet.TabIndex = 2;
+            btnevet.Text = "EVET";
+            btnevet.UseVisualStyleBackColor = false;
+            btnevet.Click += btnevet_Click;
+            // 
+            // btnhayir
+            // 
+            btnhayir.BackColor = Color.White;
+            btnhayir.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold);
+            btnhayir.Location = new Point(443, 654);
+            btnhayir.Name = "btnhayir";
+            btnhayir.Size = new Size(179, 68);
+            btnhayir.TabIndex = 3;
+            btnhayir.Text = "HAYIR";
+            btnhayir.UseVisualStyleBackColor = false;
+            btnhayir.MouseMove += btnhayir_MouseMove;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(473, 292);
+            label3.Name = "label3";
+            label3.Size = new Size(755, 39);
+            label3.TabIndex = 4;
+            label3.Text = "İNSAN KAYNAKLARI MEMNUNİYET ANKETİ";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImageLayout = ImageLayout.None;
+            pictureBox2.BorderStyle = BorderStyle.Fixed3D;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(1449, 736);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(385, 243);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            progressBar1.BackColor = Color.Blue;
+            progressBar1.ForeColor = Color.Blue;
+            progressBar1.Location = new Point(419, 174);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(1359, 82);
+            progressBar1.TabIndex = 7;
+            progressBar1.Value = 6;
+            // 
+            // lblmemnuniyet
+            // 
+            lblmemnuniyet.AutoSize = true;
+            lblmemnuniyet.BackColor = Color.Transparent;
+            lblmemnuniyet.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold);
+            lblmemnuniyet.ForeColor = Color.Yellow;
+            lblmemnuniyet.Location = new Point(852, 78);
+            lblmemnuniyet.Name = "lblmemnuniyet";
+            lblmemnuniyet.Size = new Size(320, 39);
+            lblmemnuniyet.TabIndex = 8;
+            lblmemnuniyet.Text = "Memnuniyet Oranı:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(16F, 27F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.CadetBlue;
+            BackgroundImageLayout = ImageLayout.None;
+            ClientSize = new Size(1842, 1055);
+            Controls.Add(lblmemnuniyet);
+            Controls.Add(progressBar1);
+            Controls.Add(pictureBox2);
+            Controls.Add(label3);
+            Controls.Add(btnhayir);
+            Controls.Add(btnevet);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Cursor = Cursors.Hand;
+            Font = new Font("Georgia", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            Margin = new Padding(6, 4, 6, 4);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            Text = "ZAM DEĞERLENDİRME FORMU";
+            WindowState = FormWindowState.Maximized;
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button btnevet;
+        private Button btnhayir;
+        private Label label3;
+        private PictureBox pictureBox2;
+        private ProgressBar progressBar1;
+        private Label lblmemnuniyet;
+    }
+}
---
+++ Zam/Form1.cs
@@ -0,0 +1,115 @@
+﻿namespace Zam
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int i = 1;
+        string[] sorular = { "x", "Maaşınızdan memnunmusunuz?",
+            "Şirket ve Yöneticileriniz Size Değer veriyormu?", 
+            "Pozisyonunuzdan memnunmusunuz?",
+            "Zam Oranlarınızdan memnunmuzunuz?",
+            "%50 Üstü enflasyonist ortamda sizce zam oranınız kaç olmalı?",
+            "Aynı şartlarda önümüzdeki yıllarda çalışmaya devam etmek istiyormusunuz?"};
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "SORU" + ":" + i.ToString();
+            label2.Text = sorular[i].ToString();
+            pictureBox2.Visible = false;
+            progressBar1.Value = 0;
+
+            Point p = new Point();
+            p.X = 443;
+            p.Y = 654;
+            btnhayir.Location = p;
+
+            Point p1 = new Point();
+            p1.X = 1049;
+            p1.Y = 654;
+            btnevet.Location = p1;
+
+
+        }
+
+        private void btnevet_Click(object sender, EventArgs e)
+
+        {          
+           
+
+            Point p = new Point();
+            p.X = 443;
+            p.Y = 654;
+            btnhayir.Location = p;
+
+            Point p1 = new Point();
+            p1.X = 1049;
+            p1.Y = 654;
+            btnevet.Location = p1;
+
+            double orn = i * 16.67;
+            
+                progressBar1.Value = Convert.ToInt16(orn);
+                lblmemnuniyet.Text = "Memnuniyet Oranı:" + "  " +"%"+" "+ progressBar1.Value.ToString();
+            
+
+            i = i + 1;
+            if (i < 7)
+            {
+               
+
+               
+
+
+                label1.Text = "SORU" +" "+  i.ToString()+" "+":" ;
+                label2.Text = sorular[i].ToString();
+                
+                if (i == 5)
+                {
+                    btnevet.Text = "%10 ve Altı";
+                    btnhayir.Text = "%50 ve Üstü";
+                    btnhayir.AutoSize = true;
+                    btnevet.AutoSize = true;
+                    btnevet.BackColor=Color.White;
+                    btnhayir.BackColor=Color.White;
+
+                }
+
+                if (i == 6)
+                {
+                    btnevet.Text = "TABİKİ EVET!!!";
+                    btnevet.AutoSize= true;
+                    btnhayir.Text = "HAYIR";
+                }
+            }
+
+            else
+            {
+                label1.ForeColor = Color.Blue;
+                label2.ForeColor = Color.White;
+                label1.Text = "SONUÇ" + " " + ":";
+                label2.Text = "BİZDE AYNEN BÖYLE DÜŞÜNDÜK :)"+ "\n" + "\n"+
+                    "Tebrikler tam aradığımız personelsiniz!!!" + "\n" + "\n" + 
+                    "İşinize kaldığınız yerden devam edebilirsiniz!" + "\n" + "\n" + 
+                    "Sizin Gibi Özverili ve Zeki Bir Çalışan Kolay Bulunmuyor! :)" + "\n" + "\n" +
+                    "                       "+                    "SEVGİLERLE İNSAN KAYNAKLARI...";
+                btnevet.Visible = false;
+                btnhayir.Visible = false;
+                pictureBox2.Visible = true;
+            }
+
+        }
+
+        private void btnhayir_MouseMove(object sender, MouseEventArgs e)
+        {
+            Random rd = new Random();
+            Point konum=new Point();
+            konum.X=rd.Next(250,600);
+            konum.Y = rd.Next(50, 600);
+            btnhayir.Location = konum;
+        }
+    }
+}
